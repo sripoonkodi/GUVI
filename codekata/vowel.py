5670968bfea3bf630768320576a11c23f1355464
sripoonkodi <51672672+sripoonkodi@users.noreply.github.com>
2019-06-12 10:43:29 +0530

    Update vowel.py

--- codekata/vowel.py
+++ codekata/vowel.py
@@ -1,20 +1,5 @@
 k=input()
 if ((k=='a')|(k=='e')|(k=='i')|(k=='o')|(k=='u')|(k=='A')|(k=='E')|(k=='I')|(k=='O')|(k=='U')):
   print("Vowel")
-elif ((k=='!')|(k=='@')|(k=='#')|(k=='$')|(k=='%')|(k=='^')):
-  print("invalid")
 else:
   print ("Consonant")
-
-
-
-
-
-
-
-
-
-
-
-
-

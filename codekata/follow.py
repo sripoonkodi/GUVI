1ca496aafb4b3f02e57264a86ebd5784ac0cfc74
sripoonkodi <51672672+sripoonkodi@users.noreply.github.com>
2019-06-14 11:22:22 +0530

    Update follow.py

--- codekata/follow.py
+++ codekata/follow.py
@@ -1,10 +1,10 @@
 m=[int(x) for x in input().split()]
 n=[int(x) for x in input().split()]
-e=0
+R=0
 for i in n:
   if (i==n[1]):
-    e=1
-if (e==1):
+    R=1
+if (R==1):
   print("yes")
 else:
   print("no")

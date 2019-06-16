04c21e9b27eb5c20e94de4e8b5b622d4b90d5a38
sripoonkodi <51672672+sripoonkodi@users.noreply.github.com>
2019-06-16 23:26:57 +0530

    Create StringNumDiffer.py

---
+++ codekata/StringNumDiffer.py
@@ -0,0 +1,12 @@
+A=list(input().split())
+c=0
+Q= list(A[0])
+W= list(A[1])
+for i in range(0,min(len(Q),len(W))):
+    if Q[Q]!=W[i]: 
+      c=c+1
+c=c+(len(Q)-len(W))
+if c==int(A[2]):
+  print("yes")
+else:
+  print("no")

aed31031e3ddf5828e260f8f8eaf020fd8f9e5dc
sripoonkodi <51672672+sripoonkodi@users.noreply.github.com>
2019-06-16 20:02:08 +0530

    Create Roman.py

---
+++ codekata/Roman.py
@@ -0,0 +1,5 @@
+KKK =(input())
+LLL=["Zero","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for j in range(21):
+  if KKK==LLL[j]:
+    print(j)

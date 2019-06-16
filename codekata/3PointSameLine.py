ea015d12fc4357308f881f469ed78f7abd5b61ec
sripoonkodi <51672672+sripoonkodi@users.noreply.github.com>
2019-06-16 22:32:09 +0530

    Update 3PointSameLine.py

--- codekata/3PointSameLine.py
+++ codekata/3PointSameLine.py
@@ -1,7 +1,7 @@
 A=input().split()
 B=input().split()
 C=input().split()
-if (A[0]==B[0]==C[0] or A[1]==B[1]==C[1] or (A[0]==A[1] and B[0]==B[1] and c[0]==C[1])):
+if (A[0]==B[0]==C[0] or A[1]==B[1]==C[1] or (A[0]==A[1] and B[0]==B[1] and C[0]==C[1])):
   print('yes')
 else:
   print('no')
